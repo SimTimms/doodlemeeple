4922477c68e2e5f16c64f133ae118ea67e783b6a
Tim Simms <36262574+SimTimms@users.noreply.github.com>
2020-02-17 13:05:17 +0000

    Debug

--- src/Router.js
+++ src/Router.js
@@ -10,30 +10,6 @@ function App() {
   return (
     <Router>
       <Switch>
-        <Route
-          path="/app/:page/:pathParam?"
-          render={props => <AppLayout {...props} />}
-        />
-        <Route
-          path="/messages/:page/:pathParam?"
-          render={props => <MessagesLayout {...props} />}
-        />
-        <Route
-          path="/roles/:page/:pathParam?"
-          render={props => <RolesLayout {...props} />}
-        />
-        <Route
-          path="/message/:page/:pathParam?"
-          render={props => <MessageLayout {...props} />}
-        />
-        <Route path="/about">
-          <AboutLayoutFrame />
-        </Route>
-        <Route
-          path="/:page"
-          render={props => <PublicLayoutFrame {...props} />}
-        />
-
         <Route path="/">
           <PublicLayoutFrame />
         </Route>
