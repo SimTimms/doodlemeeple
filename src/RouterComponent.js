3766721a05aa10c2135b3a41421a52a162c046db
Tim Simms <36262574+SimTimms@users.noreply.github.com>
2020-04-29 12:27:22 +0100

    Remove mini paint

--- src/RouterComponent.js
+++ src/RouterComponent.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { AboutLayoutFrame } from './layouts';
 import AppLayout from './layouts/app';
-import { Chaos } from './layouts/chaos';
 import PreviewLayout from './layouts/preview';
 import { PublicLayout } from './layouts/public';
 import MessagesLayout from './layouts/messages';
@@ -74,9 +73,6 @@ function RouterComponent(props) {
             render={(props) => <MessageLayout {...props} />}
           />
         )}
-        <Route path="/chaos">
-          <Chaos />
-        </Route>
         <Route path="/about">
           <AboutLayoutFrame />
         </Route>
