259f5966084a383a6a5a3d765a301ed528a4e791
Tim.Simms <tim-simms@hotmail.com>
2021-02-11 10:31:15 +0000

    Fix social link missing http issue

--- src/layouts/preview/views/previewProfile/socialIcon.js
+++ src/layouts/preview/views/previewProfile/socialIcon.js
@@ -4,9 +4,13 @@ import { useStyles } from './styles';
 export default function SocialIcon({ img, link }) {
   const classes = useStyles();
 
-  return link !== '' && link !== null ? (
+  return link !== '' && link !== null && link != undefined ? (
     <a
-      href={link}
+      href={
+        link.indexOf('http') === -1 && link.indexOf('https') === -1
+          ? `https://${link}`
+          : link
+      }
       target="_blank"
       rel="noopener noreferrer"
       style={{ textDecoration: 'none' }}
