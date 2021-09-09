6cbfc416f0f8e0cb93f7244a43915bf0e8f2c243
Tim.Simms <tim-simms@hotmail.com>
2021-09-09 09:03:54 +0100

    Remove G Tracker

--- src/index.js
+++ src/index.js
@@ -2,12 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import RouterComponent from './RouterComponent';
 import { BrowserRouter as Router } from 'react-router-dom';
-import ReactGA from 'react-ga';
-
-const trackingId = 'G-TGWQGWN3D2'; // Replace with your Google Analytics tracking ID
-ReactGA.initialize(trackingId);
-
-ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(
   <Router>
